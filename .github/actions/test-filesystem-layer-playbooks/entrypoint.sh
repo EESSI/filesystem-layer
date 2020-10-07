acd7cfa70eea98abdcd690649059dd2614400b93
Bob Dröge <b.e.droge@rug.nl>
2020-10-07 13:16:45 +0200

    manually start docker

--- .github/actions/test-filesystem-layer-playbooks/entrypoint.sh
+++ .github/actions/test-filesystem-layer-playbooks/entrypoint.sh
@@ -10,8 +10,9 @@ EOF
 #systemctl status httpd
 #systemctl start httpd
 #systemctl status httpd
-echo "systemd" > /proc/1/comm
+#echo "systemd" > /proc/1/comm
 
 ansible-galaxy role install -r requirements.yml -p ./roles
-ansible-playbook --connection=local -v ${GITHUB_WORKSPACE}/$1
+ansible-playbook --connection=local -v ${GITHUB_WORKSPACE}/stratum0.yml
+#ansible-playbook --connection=local -v ${GITHUB_WORKSPACE}/$1
 #ansible-playbook --connection=local -e ansible_python_interpreter=python3 ${GITHUB_WORKSPACE}/$1
