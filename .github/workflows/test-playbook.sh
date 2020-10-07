9c6a09f9425f25fd96b8c548405b868307aa055e
Bob Dröge <b.e.droge@rug.nl>
2020-10-07 16:41:17 +0200

    Skip GEO DB configuration

--- .github/workflows/test-playbook.sh
+++ .github/workflows/test-playbook.sh
@@ -6,5 +6,7 @@ hostgroup=$(grep hosts $playbook | awk '{print $2}')
 echo "[$hostgroup]" > inventory/hosts
 echo "127.0.0.1" >> inventory/hosts
 
+export CVMFS_GEO_DB_FILE=NONE
+
 ansible-galaxy role install -r requirements.yml -p ./roles
 ansible-playbook --connection=local -v $1
