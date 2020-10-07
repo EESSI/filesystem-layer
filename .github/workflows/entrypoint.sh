93e6f3cb0a7bab92220a7c8d0d8d1b9274a14a7f
Bob Dröge <b.e.droge@rug.nl>
2020-10-07 15:06:23 +0200

    workdir is github_workspace

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-cd ${GITHUB_WORKSPACE}
+#cd ${GITHUB_WORKSPACE}
 
 cat << EOF > inventory/hosts
 [cvmfsstratum0servers]
@@ -13,6 +13,6 @@ EOF
 #echo "systemd" > /proc/1/comm
 
 ansible-galaxy role install -r requirements.yml -p ./roles
-ansible-playbook --connection=local -v ${GITHUB_WORKSPACE}/stratum0.yml
+ansible-playbook --connection=local -v stratum0.yml
 #ansible-playbook --connection=local -v ${GITHUB_WORKSPACE}/$1
 #ansible-playbook --connection=local -e ansible_python_interpreter=python3 ${GITHUB_WORKSPACE}/$1
