0366ca8670f55c12f43ae90d41b296aad02a1fcf
Bob Dröge <b.e.droge@rug.nl>
2020-10-08 11:05:34 +0200

    make os variable, only make snapshots of config repo for stratum1

--- .github/workflows/test-playbook.sh
+++ .github/workflows/test-playbook.sh
@@ -6,7 +6,14 @@ hostgroup=$(grep hosts $playbook | awk '{print $2}')
 echo "[$hostgroup]" > inventory/hosts
 echo "127.0.0.1" >> inventory/hosts
 
+touch inventory/local_site_specific_vars.yml
+
 export CVMFS_GEO_DB_FILE=NONE
 
+if [ $playbook == "stratum1.yml" ]
+then
+  echo 'cvmfs_repositories: "[{{ eessi_cvmfs_config_repo.repository }}]"' >> inventory/local_site_specific_vars.yml
+fi
+
 ansible-galaxy role install -r requirements.yml -p ./roles
-ansible-playbook --connection=local -v $1
+ansible-playbook --connection=local -e @inventory/local_site_specific_vars.yml ${playbook}
