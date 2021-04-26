28569c86d8e60dc8326530815a511aa1d2a7b05d
Bob Dröge <b.e.droge@rug.nl>
2021-04-26 09:05:16 +0200

    remove old arch-specific dockerfiles and push script

--- containers/docker_build_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-os="centos7"
-cpu_arch=$(uname -m)
-tag="eessi/client-pilot:${os}-${cpu_arch}"
-
-docker build --no-cache -f Dockerfile.EESSI-client-pilot-${os}-${cpu_arch} -t ${tag} .
-
-docker push ${tag}
