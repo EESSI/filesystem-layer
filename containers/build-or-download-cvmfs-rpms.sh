f0b68135671df1950f2823858557f5e3452e5e02
Bob Dröge <b.e.droge@rug.nl>
2021-04-29 13:02:45 +0200

    add comments, trigger rebuild to test cache

--- containers/build-or-download-cvmfs-rpms.sh
+++ containers/build-or-download-cvmfs-rpms.sh
@@ -4,10 +4,12 @@ arch=$(uname -m)
 yum install -y wget
 if [ "$arch" = "ppc64le" ]
 then
+    # build RPMs using the source RPMs
     yum install -y epel-release
     yum install -y rpm-build checkpolicy cmake fuse-devel fuse3-devel gcc gcc-c++ golang libattr-devel libcap-devel libuuid-devel openssl-devel python2-devel python2-setuptools selinux-policy-devel valgrind-devel hardlink selinux-policy-targeted
     wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/cvmfs-${cvmfsversion}-1.el7.src.rpm && rpmbuild --rebuild cvmfs-${cvmfsversion}-1.el7.src.rpm
 else
+    # just download the available RPMs
     mkdir -p /root/rpmbuild/RPMS/${arch}
     cd /root/rpmbuild/RPMS/${arch}
     wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/cvmfs-${cvmfsversion}-1.el7.${arch}.rpm
