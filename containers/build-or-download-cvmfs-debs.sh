6dec944ef40a2441db984b39f419ec4e467edb04
Bob Dröge <b.e.droge@rug.nl>
2021-06-04 13:15:13 +0200

    remove redundant download, fix dir path to cvmfs

--- containers/build-or-download-cvmfs-debs.sh
+++ containers/build-or-download-cvmfs-debs.sh
@@ -7,10 +7,9 @@ if [ "$arch" = "ppc64el" ] || [ "$arch" = "arm64" ]
 then
     apt-get install devscripts libfuse3-dev cmake cpio libcap-dev libssl-dev libfuse-dev pkg-config libattr1-dev python-dev python-setuptools uuid-dev valgrind libz-dev
     cd /tmp
-    wget http://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/source.tar.gz
     wget https://github.com/cvmfs/cvmfs/archive/refs/tags/cvmfs-${cvmfsversion}.tar.gz
     tar xzf cvmfs-${cvmfsversion}.tar.gz
-    cd cvmfs-${cvmfsversion}/ci/cvmfs    
+    cd cvmfs-cvmfs-${cvmfsversion}/ci/cvmfs
     mkdir /root/deb
     ./deb.sh /tmp/cvmfs-cvmfs-${cvmfsversion} /root/deb
 else
