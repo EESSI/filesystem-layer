5e4a40c4f4f6c60832c6c12f43051c529cc6478c
Bob Dröge <b.e.droge@rug.nl>
2021-09-03 17:59:22 +0200

    check config file for expected items

--- scripts/automated_ingestion/automated_ingestion.py
+++ scripts/automated_ingestion/automated_ingestion.py
@@ -11,6 +11,13 @@ import logging
 import os
 import sys
 
+REQUIRED_CONFIG = {
+    'secrets': ['aws_secret_access_key', 'aws_access_key_id', 'github_pat'],
+    'paths': ['download_dir', 'ingestion_script', 'metadata_file_extension'],
+    'aws': ['staging_bucket'],
+    'github': ['staging_repo', 'failed_ingestion_issue_body', 'pr_body'],
+}
+
 
 def error(msg, code=1):
     logging.error(msg)
@@ -26,12 +33,19 @@ def find_tarballs(s3, bucket, extension='.tar.gz'):
     return tarballs
 
 
-def read_config(path):
+def parse_config(path):
     config = configparser.ConfigParser()
     try:
         config.read(path)
     except:
         error(f'Unable to read configuration file {path}!')
+
+    for section in REQUIRED_CONFIG.keys():
+        if not section in config:
+            error(f'Missing section "{section}" in configuration file {path}.')
+        for item in REQUIRED_CONFIG[section]:
+            if not item in config[section]:
+                error(f'Missing configuration item "{item}" in section "{section}" of configuration file {path}.')
     return config
 
 
@@ -46,7 +60,7 @@ def parse_args():
 def main():
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     args = parse_args()
-    config = read_config(args.config)
+    config = parse_config(args.config)
     # TODO: check configuration: secrets, paths, permissions on dirs, etc
     gh_pat = config['secrets']['github_pat']
     gh = github.Github(gh_pat)
