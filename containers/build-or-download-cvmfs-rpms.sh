7c7668c694b4f31d3b5be1d10803c4723d0da0d3
Bob Dröge <b.e.droge@rug.nl>
2022-07-22 10:10:36 +0200

    only install python3

--- containers/build-or-download-cvmfs-rpms.sh
+++ containers/build-or-download-cvmfs-rpms.sh
@@ -5,7 +5,7 @@ yum install -y wget
 if [ "$arch" = "ppc64le" ]
 then
     yum install -y epel-release
-    yum install -y rpm-build checkpolicy cmake fuse-devel fuse3-devel gcc gcc-c++ golang libattr-devel libcap-devel libuuid-devel openssl-devel python2-devel python2-setuptools selinux-policy-devel valgrind-devel hardlink selinux-policy-targeted
+    yum install -y rpm-build checkpolicy cmake fuse-devel fuse3-devel gcc gcc-c++ golang libattr-devel libcap-devel libuuid-devel openssl-devel python3-devel python3-setuptools selinux-policy-devel valgrind-devel hardlink selinux-policy-targeted
     wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/cvmfs-${cvmfsversion}-1.el7.src.rpm && rpmbuild --rebuild cvmfs-${cvmfsversion}-1.el7.src.rpm
 else
     mkdir -p /root/rpmbuild/RPMS/${arch}
