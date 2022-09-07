5c7f026a18b546edb54cb38515ae25d17f5a0012
Bob Dröge <b.e.droge@rug.nl>
2022-09-07 15:41:07 +0200

    refactored the script

--- scripts/ingest-tarball.sh
+++ scripts/ingest-tarball.sh
@@ -15,6 +15,13 @@
 repo=pilot.eessi-hpc.org
 basedir=versions
 decompress="gunzip -c"
+# list of supported architectures for compat and software layers
+declare -A archs=(["aarch64"]= ["ppc64le"]= ["riscv64"]= ["x86_64"]=)
+# list of supported operating systems for compat and software layers
+declare -A oss=(["linux"]= ["macos"]=)
+# list of supported tarball content types
+declare -A content_types=(["compat"]= ["init"]= ["software"]=)
+
 
 function echo_green() {
     echo -e "\e[32m$1\e[0m"
@@ -33,6 +40,128 @@ function error() {
     exit 1
 }
 
+function check_version() {
+    # Check if the EESSI version number encoded in the filename
+    # is valid, i.e. matches the format YYYY.DD
+    if ! echo "${version}" | egrep -q '^20[0-9][0-9]\.(0[0-9]|1[0-2])$'
+    then
+        error "${version} is not a valid EESSI version."
+    fi
+
+    # Check if the version encoded in the filename matches the top-level dir inside the tarball
+    if [ "${version}" != "${tar_top_level_dir}" ]
+    then
+        error "the version in the filename (${version}) does not match the top-level directory in the tarball (${tar_top_level_dir})."
+    fi
+}
+
+function check_contents_type() {
+    # Check if the name of the second-level dir in the tarball matches to what is specified in the filename
+    if [ "${contents_type_dir}" != "${tar_contents_type_dir}" ]
+    then
+        error "the contents type in the filename (${contents_type_dir}) does not match the contents type in the tarball (${tar_contents_type_dir})."
+    fi
+
+    # Check if the second-level dir in the tarball is compat, software, or init
+    if [ ! -v content_types[${tar_contents_type_dir}] ]
+    then
+        error "the second directory level of the tarball contents should be either compat, software, or init."
+    fi
+}
+
+function cvmfs_ingest_tarball() {
+    # Do a regular "cvmfs_server ingest" for a given tarball,
+    # followed by regenerating the nested catalog
+    echo "Ingesting tarball ${tar_file} to ${repo}..."
+    ${decompress} "${tar_file}" | cvmfs_server ingest -t - -b "${basedir}" "${repo}"
+    ec=$?
+    if [ $ec -eq 0 ]
+    then
+        echo_green "${tar_file} has been ingested to ${repo}."
+    else
+        error "${tar_file} could not be ingested to ${repo}."
+    fi
+
+    # Use the .cvmfsdirtab to generate nested catalogs for the ingested tarball
+    # ("cvmfs_server ingest" doesn't do this automatically)
+    echo "Generating the nested catalogs..."
+    cvmfs_server transaction "${repo}"
+    cvmfs_server publish -m "Generate catalogs after ingesting ${tar_file_basename}" "${repo}"
+    ec=$?
+    if [ $ec -eq 0 ]
+    then
+        echo_green "Nested catalogs for ${repo} have been created!"
+    else
+        echo_red "failure when creating nested catalogs for ${repo}."
+    fi
+}
+
+function check_os() {
+    # Check if the operating system directory is correctly set for the contents of the tarball
+    os=$(echo "${tar_first_file}" | cut -d / -f 3)
+    if [ -z "${os}" ]
+    then
+        error "No operating system directory found in the tarball!"
+    fi
+    if [ ! -v oss[${os}] ]
+    then
+        error "The operating system directory in the tarball is ${os}, which is not a valid operating system!"
+    fi
+}
+
+function check_arch() {
+    # Check if the architecture directory is correctly set for the contents of the tarball
+    arch=$(echo "${tar_first_file}" | cut -d / -f 4)
+    if [ -z "${arch}" ]
+    then
+        error "No architecture directory found in the tarball!"
+    fi
+    if [ ! -v archs[${arch}] ]
+    then
+        error "The architecture directory in the tarball is ${arch}, which is not a valid architecture!"
+    fi
+}
+
+function ingest_init_tarball() {
+    # Handle the ingestion of tarballs containing init scripts
+    cvmfs_ingest_tarball
+}
+
+function ingest_software_tarball() {
+    # Handle the ingestion of tarballs containing software installations
+    check_arch
+    check_os
+    cvmfs_ingest_tarball
+}
+
+function ingest_compat_tarball() {
+    # Handle the ingestion of tarballs containing a compatibility layer
+    check_arch
+    check_os
+    # Assume that we already had a compat layer in place if there is a startprefix script in the corresponding CVMFS directory
+    if [ -f "/cvmfs/${repo}/${basedir}/${version}/compat/${os}/${arch}/startprefix" ];
+    then
+        echo_yellow "Compatibility layer for version ${version}, OS ${os}, and architecture ${arch} already exists!"
+        echo_yellow "Removing the existing layer, and adding the new one from the tarball..."
+        cvmfs_server transaction "${repo}"
+        rm -rf "/cvmfs/${repo}/${basedir}/${version}/compat/${os}/${arch}/"
+        tar -C "/cvmfs/${repo}/${basedir}/" -xzf "${tar_file}"
+        cvmfs_server publish -m "update compat layer for ${version}, ${os}, ${arch}" "${repo}"
+        ec=$?
+        if [ $ec -eq 0]
+        then
+            echo_green "Successfully ingested the new compatibility layer!"
+        else
+            cvmfs_server abort "${repo}"
+            error "Error while updating the compatibility layer, transaction aborted."
+        fi
+    else
+        cvmfs_ingest_tarball
+    fi
+
+}
+
+
 # Check if a tarball has been specified
 if [ "$#" -ne 1 ]; then
     error "usage: $0 <gzipped tarball>"
@@ -45,87 +174,15 @@ if [ ! -f "${tar_file}" ]; then
     error "tar file ${tar_file} does not exist!"
 fi
 
+# Get some information about the tarball
 tar_file_basename=$(basename "${tar_file}")
-version=$(echo ${tar_file_basename} | cut -d- -f2)
-contents_type_dir=$(echo ${tar_file_basename} | cut -d- -f3)
-tar_top_level_dir=$(tar tf "${tar_file}" | head -n 1 | cut -d/ -f1)
-# Use the 2nd file/dir in the tarball, as the first one may be just "<version>/"
-tar_contents_type_dir=$(tar tf "${tar_file}" | head -n 2 | tail -n 1 | cut -d/ -f2)
+version=$(echo "${tar_file_basename}" | cut -d- -f2)
+contents_type_dir=$(echo "${tar_file_basename}" | cut -d- -f3)
+tar_first_file=$(tar tf "${tar_file}" | head -n 1)
+tar_top_level_dir=$(echo "${tar_first_file}" | cut -d/ -f1)
+tar_contents_type_dir=$(echo "${tar_first_file}" | head -n 2 | tail -n 1 | cut -d/ -f2)
 
-# Check if the EESSI version number encoded in the filename
-# is valid, i.e. matches the format YYYY.DD
-if ! echo "${version}" | egrep -q '^20[0-9][0-9]\.(0[0-9]|1[0-2])$'
-then
-    error "${version} is not a valid EESSI version."
-fi
-
-# Check if the version encoded in the filename matches the top-level dir inside the tarball
-if [ "${version}" != "${tar_top_level_dir}" ]
-then
-    error "the version in the filename (${version}) does not match the top-level directory in the tarball (${tar_top_level_dir})."
-fi
-
-# Check if the second-level dir in the tarball is compat, software, or init
-if [ "${tar_contents_type_dir}" != "compat" ] && [ "${tar_contents_type_dir}" != "software" ] && [ "${tar_contents_type_dir}" != "init" ]
-then
-    error "the second directory level of the tarball contents should be either compat, software, or init."
-fi
-
-# Check if the name of the second-level dir in the tarball matches to what is specified in the filename
-if [ "${contents_type_dir}" != "${tar_contents_type_dir}" ]
-then
-    error "the contents type in the filename (${contents_type_dir}) does not match the contents type in the tarball (${tar_contents_type_dir})."
-fi
-
-# If this is a compat layer tarball, we need to check if it's an update, and if so, use a special procedure
-if [ "${tar_contents_type_dir}" = "compat" ]
-then
-    tar_first_file="$(tar tf "${tar_file}" | head -n 1)"
-    # Get OS and architecture from path, which should look like: <version>/compat/<os>/<arch>
-    compat_os=$(echo "${tar_first_file}" | cut -d / -f 3)
-    compat_arch=$(echo "${tar_first_file}" | cut -d / -f 4)
-    # Assume that we already had a compat layer in place if there is a startprefix script in the corresponding CVMFS directory
-    if [ -f "/cvmfs/${repo}/${basedir}/${version}/compat/${compat_os}/${compat_arch}/startprefix" ];
-    then
-        echo_yellow "Compatibility layer for version ${version}, OS ${compat_os}, and architecture ${compat_arch} already exists!"
-        echo_yellow "Removing the existing layer, and adding the new one from the tarball..."
-        cvmfs_server transaction "${repo}"
-        rm -rf "/cvmfs/${repo}/${basedir}/${version}/compat/${compat_os}/${compat_arch}/"
-        tar -C "/cvmfs/${repo}/${basedir}/" -xzf "${tar_file}"
-        cvmfs_server publish -m "update compat layer for ${version}, ${compat_os}, ${compat_arch}" "${repo}"
-        ec=$?
-        if [ $ec -eq 0]
-        then
-            echo_green "Successfully ingested the new compatibility layer!"
-            # As the publish operation already created new nested catalogs, we can just exit now
-            exit 0
-        else
-            cvmfs_server abort "${repo}"
-            error "Error while updating the compatibility layer, transaction aborted."
-        fi
-    fi
-fi
-
-# For other tarballs, just ingest them using the "cvmfs_server ingest" command
-echo "Ingesting tarball ${tar_file} to ${repo}..."
-${decompress} "${tar_file}" | cvmfs_server ingest -t - -b "${basedir}" "${repo}"
-ec=$?
-if [ $ec -eq 0 ]
-then
-    echo_green "${tar_file} has been ingested to ${repo}."
-else
-    error "${tar_file} could not be ingested to ${repo}."
-fi
-
-# Use the .cvmfsdirtab to generate nested catalogs for the ingested tarball
-# ("cvmfs_server ingest" doesn't do this automatically)
-echo "Generating the nested catalogs..."
-cvmfs_server transaction "${repo}"
-cvmfs_server publish -m "Generate catalogs after ingesting ${tar_file_basename}" "${repo}"
-ec=$?
-if [ $ec -eq 0 ]
-then
-    echo_green "Nested catalogs for ${repo} have been created!"
-else
-    echo_red "failure when creating nested catalogs for ${repo}."
-fi
+# Do some checks, and ingest the tarball
+check_version
+check_contents_type
+ingest_${tar_contents_type_dir}_tarball
