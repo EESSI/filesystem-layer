90ddc53936e4ac4df797c6bc872dd9c73e092956
Bob Dröge <b.e.droge@rug.nl>
2023-05-18 11:40:45 +0200

    use 2nd file for determining tarball type

--- scripts/ingest-tarball.sh
+++ scripts/ingest-tarball.sh
@@ -220,7 +220,7 @@ fi
 tar_file_basename=$(basename "${tar_file}")
 version=$(echo "${tar_file_basename}" | cut -d- -f2)
 contents_type_dir=$(echo "${tar_file_basename}" | cut -d- -f3)
-tar_first_file=$(tar tf "${tar_file}" | head -n 1)
+tar_first_file=$(tar tf "${tar_file}" | head -n 2 | tail -n 1)
 tar_top_level_dir=$(echo "${tar_first_file}" | cut -d/ -f1)
 tar_contents_type_dir=$(echo "${tar_first_file}" | head -n 2 | tail -n 1 | cut -d/ -f2)
 
