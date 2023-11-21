bc358b9bf883f45d359ca055d702e4a2f484c3b7
Bob Dröge <b.e.droge@rug.nl>
2023-11-21 20:28:27 +0100

    download new EESSI config package

--- containers/build-or-download-cvmfs-debs.sh
+++ containers/build-or-download-cvmfs-debs.sh
@@ -28,3 +28,4 @@ fi
 
 cd /root/deb
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default_latest_all.deb
+wget https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi_latest_all.deb
