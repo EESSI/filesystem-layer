d4528fef653485cbef13546c5eea18fe581542f1
Thomas Röblitz <thomas.roblitz@uib.no>
2025-06-21 12:24:47 +0200

    bump sequence number to 9

--- scripts/automated_ingestion/eessi_task.py
+++ scripts/automated_ingestion/eessi_task.py
@@ -262,7 +262,7 @@ class EESSITask:
         """
         Get a fixed sequence number.
         """
-        return 8
+        return 9
 
     @log_function_entry_exit()
     def _determine_sequence_status(self, sequence_number: int = None) -> int:
