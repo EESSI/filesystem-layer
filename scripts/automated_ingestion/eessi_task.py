80897fc86a231103d4caa448c9c96bc46a4ab7ea
Thomas Roeblitz <thomas.roblitz@uib.no>
2025-08-06 14:05:31 +0200

    remove unused type

--- scripts/automated_ingestion/eessi_task.py
+++ scripts/automated_ingestion/eessi_task.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 from functools import total_ordering
-from typing import Dict, List, Tuple, Optional, Any
+from typing import Dict, List, Optional, Any
 
 import base64
 import os
