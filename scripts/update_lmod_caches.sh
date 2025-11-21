dd1ce43727e4f6791ebfc27aac9a3913141173f1
Bob Dröge <b.e.droge@rug.nl>
2025-11-21 15:06:52 +0100

    allow overriding of the Lmod cache update script location

--- scripts/update_lmod_caches.sh
+++ scripts/update_lmod_caches.sh
@@ -14,11 +14,12 @@ function error() {
 }
 
 # Check if a stack base dir has been specified
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
     error "usage: $0 <path to main directory of an EESSI stack>"
 fi
 
 stack_base_dir="$1"
+update_lmod_system_cache_files="$2"
 
 # Check if the given stack base dir exists
 if [ ! -d ${stack_base_dir} ]
@@ -26,8 +27,11 @@ then
   error "${stack_base_dir} does not point to an existing directory!"
 fi
 
-# Check if Lmod's cache update script can be found at the expected location (in the compatibility layer of the given stack)
-update_lmod_system_cache_files="${stack_base_dir}/compat/linux/$(uname -m)/usr/share/Lmod/libexec/update_lmod_system_cache_files"
+# If no Lmod cache update script was specified, try to find one in the compatibility layer of the given stack
+if [ -z ${update_lmod_system_cache_files} ]; then
+  update_lmod_system_cache_files="${stack_base_dir}/compat/linux/$(uname -m)/usr/share/Lmod/libexec/update_lmod_system_cache_files"
+fi
+# Make sure that the expected Lmod cache update script exists
 if [ ! -f ${update_lmod_system_cache_files} ]
 then
   error "expected to find Lmod's cache update script at ${update_lmod_system_cache_files}, but it doesn't exist."
